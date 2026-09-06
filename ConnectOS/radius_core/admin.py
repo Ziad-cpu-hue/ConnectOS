efc375764415ff560d17d64e6c8e80cf9ba90a90
Ziad-cpu-hue <code37265@gmail.com>
2026-09-06 11:36:47 +0300

    Delete ConnectOS/radius_core directory

--- ConnectOS/radius_core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import RadAcct, RadCheck, RadNas, RadReply
-
-admin.site.register(RadCheck)
-admin.site.register(RadReply)
-admin.site.register(RadNas)
-admin.site.register(RadAcct)
--- ConnectOS/radius_core/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class RadiusCoreConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "radius_core"
-    verbose_name = "جسر RADIUS"
